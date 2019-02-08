311794214adc3fbf6a4247fc110192f308e6e027
Matthew Eisenstadter <mattieice@gmail.com>
2019-02-08 08:35:16 -0500

    fixed security vulnerability

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 var Word = require("./word.js");
+
+inquirer.prompt([
+
+    {
+      type: "input",
+      name: "userInput",
+      message: "Which location or landmark would you like to geocode?"
+    }
+  
+  ]).then(function(location) {
+  
+  
+
+  
+  });
+  
